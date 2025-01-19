09bbb2e0090dd36193daf74aacdc4bad2458a878
Jed Cruz <135231381+Augustus1417@users.noreply.github.com>
2025-01-19 10:16:56 +0000

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+--
+-- Load Catppuccin Mocha with transparency
+require("catppuccin").setup({
+  flavour = "mocha",
+  transparent_background = true,
+})
+
+-- Apply the theme
+vim.cmd("colorscheme catppuccin")
+vim.cmd("highlight CursorLine guibg=NONE")
+
+if vim.g.neovide then
+  local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  end
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 0.9
+  vim.g.neovide_background_color = "#8caaee" .. alpha()
+  vim.cmd("colorscheme tokyonight")
+  vim.g.tokyonight_style = "storm"
+  vim.g.neovide_cursor_trail_size = 0.2
+end
